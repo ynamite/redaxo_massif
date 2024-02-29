9abc30d255f0c7848d185df24c2d60e472d4e22b
ynamite <ynamite@bluewin.ch>
2024-02-29 16:45:47 +0100

    chore: cleanup

--- fragments/massif-swiper.php
+++ fragments/massif-swiper.php
@@ -5,14 +5,18 @@ $params = $this->params['params'];
     <div class="swiper-wrapper" <?php if (rex::isBackend()) echo ' style="list-style:none; margin: 0; padding: 0; display: grid; grid-gap: 8px;grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));"'; ?>>
         <?php if ($params['content']) echo $params['content'];
         else foreach ($params['images'] as $key => $img) {
-            echo massif_utils::parse('swiper-slide', null, ['idx' => $key, 'content' => rex_media_plus::get($img)->setAttributes(['loading="lazy"'])->getPicture('default')]);
+            echo massif_utils::parse('swiper-slide', null, ['idx' => $key, 'content' => massif_img::get($img, ['width' => 371, 'loading' => $idx == 0 ? 'eager' : 'lazy'])]);
         } ?>
     </div>
     <?php if ($params['controls'] && rex::isFrontend()) { ?>
         <div class="swiper-controls">
             <?php if ($params['dir-nav']) { ?>
-                <div class="swiper-button-prev"><?php require(theme_path::assets('img/ico-arrow-lg-right.svg')) ?></div>
-                <div class="swiper-button-next"><?php require(theme_path::assets('img/ico-arrow-lg-right.svg')) ?></div>
+                <div class="swiper-button-prev">
+                    <i class="icon icon-[ion--chevron-left]"></i>
+                </div>
+                <div class="swiper-button-next ">
+                    <i class="icon icon-[ion--chevron-right]"></i>
+                </div>
             <?php } ?>
             <?php if ($params['pager']) { ?>
                 <div class="swiper-pagination"></div>
