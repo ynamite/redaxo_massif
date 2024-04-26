bde7753173c9221c494bf1f8ac598900e0412c74
ynamite <ynamite@bluewin.ch>
2024-04-26 16:01:23 +0200

    fix: massif-section

--- fragments/massif-section.php
+++ fragments/massif-section.php
@@ -1,6 +1,6 @@
 <?php
 $category = $this->getVar('category', null);
-$ctx = $this->getVar('ctx', null);
+$instance = $this->getVar('this', null);
 
 if (!$category) {
   $pathRoute = massif_utils::getPathRoute();
@@ -9,19 +9,20 @@ if (!$category) {
     if (!$category) {
       $category = rex_article::getCurrent();
     }
-  } else if ($ctx) {
-    echo $ctx->getArticle();
+  } else if ($instance) {
+    echo $instance->getArticle();
   }
 }
 if ($category) {
 
+  $id = rex_string::normalize($category->getName());
   $article_helper = rex_article::get($category->getId(), 1);
   $content = (new rex_article_content($category->getId()))->getArticle(1);
 
   $name = rex_string::normalize($article_helper->getName());
   $isHome = ($category->getId() == rex_article::getSiteStartArticleId()) ? true : false;
 ?>
-  <div data-section id="sec-<?= $name ?>" class="section section-<?= $name ?>">
+  <div data-section id="sec-<?= $id ?>" class="section section-<?= $name ?>">
     <?= $content ?>
   </div>
 <?php } ?>
